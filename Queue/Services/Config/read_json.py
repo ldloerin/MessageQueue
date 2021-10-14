b7b0077612394764647db91923704d3b752bee21
David Loerinci <david.loerinci@gmail.com>
2021-10-14 17:01:42 +0200

    changed error report

--- Queue/Services/Config/read_json.py
+++ Queue/Services/Config/read_json.py
@@ -7,12 +7,13 @@ class ReadJson:
     def read_json_file(json_file):
         if os.path.isfile(json_file):
             try:
-                with open(json_file, encoding = "utf-8") as encoded_json_file:
+                with open(json_file, encoding="utf-8") as encoded_json_file:
                     inputs = json.load(encoded_json_file)
                     return inputs
-            except:
-                warning_message = "Warning: Could not read " + json_file
+            except Exception as e:
+                warning_message = "\n\nWarning: Could not read " + json_file + "\n"
+                warning_message += str(e) + "\n\n"
                 sys.exit(warning_message)
         else:
-            warning_message = "Warning: Could not find " + json_file
+            warning_message = "\n\nWarning: Could not find " + json_file + "\n\n"
             sys.exit(warning_message)
